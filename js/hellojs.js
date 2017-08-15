658686d23a658c5f969c17b8faef2d3172388a91
raphaelCavalcante <raphael.cavalcante100@gmail.com>
2017-08-15 11:41:28 -0400

    change javascript

--- js/hellojs.js
+++ js/hellojs.js
@@ -1,4 +1,1 @@
-var textExample=document.createElement("h1");
-textExample.textContent="raphael";
-var div  = document.getElementById("changetext");
-div.appendChild(textExample);
+console.log("teste");
